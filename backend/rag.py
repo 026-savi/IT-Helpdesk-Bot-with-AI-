592fcd9036ff14d2f925a7b7d3611ec2e3f22327
026-savi <savisethi39@gmail.com>
2026-05-11 17:43:30 +0530

    Fixed rag imports

--- backend/rag.py
+++ backend/rag.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai.embeddings import OpenAIEmbeddings
 
 embeddings = OpenAIEmbeddings()
 
